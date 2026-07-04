63eb0ee42a5a9451951d0f4f20d2e12db20d9c65
Qoo <1969641455@qq.com>
2026-07-04 15:21:29 +0800

    Pure init UE project

---
+++ CGJ2026_Project/Source/CGJ2026/CGJ2026.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CGJ2026.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CGJ2026, "CGJ2026" );
